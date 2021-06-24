2c4f6622f43bcbeb0e69ea9253fed336c8a5b3f7
Shubham97-jain <you@example.com>
2021-06-24 12:16:16 +0530

    modify user(controler)

---
+++ controllers/user.js
@@ -0,0 +1,43 @@
+
+const User=require('../model(Databse)/user');
+exports.signup=(req,res)=>
+{
+    User.findOne({email:req.body.email})
+    .exec((error,user) =>{
+        if(user)
+        return res.status(404).json({
+            message:'User already registered'
+        });
+
+         const {
+             firstname,
+             lastname,
+             email,  
+             password
+            }=req.body;
+        const _user=new User({firstname,
+            lastname,
+            email,
+            password,
+            username:Math.random().toString()
+        });
+        _user.save((error,data)=> {
+            if(error){
+                return res.status(400).json({
+                    message:'Somthing went wrong'
+
+                });
+            }
+            if(data)
+            {
+                return res.status(201).json({
+                    //user:data   //  to get in resp
+                    message :"user created sucessfully"
+                })
+            }
+
+        });
+
+        
+    });
+}
--- routers/user.js
+++ routers/user.js
@@ -1,55 +1,18 @@
 const express=require('express');
-const User=require('../model(Databse)/user');
+const { signup } = require('../controllers/user');
+
 const router=express.Router();
 
 
-router.post('/sign',(req,res)=>{
+router.post('/signup',signup)
 
 
-});
 
-router.post('/signup',(req,res)=>{
+// router.post('/signup',(req,res)=>{
 
-    User.findOne({email:req.body.email})
-    .exec((error,user) =>{
-        if(user)
-        return res.status(404).json({
-            message:'User already registered'
-        });
-
-         const {
-             firstname,
-             lastname,
-             email,
-             password
-            }=req.body;
-        const _user=new User({firstname,
-            lastname,
-            email,
-            password,
-            username:Math.random().toString()
-        });
-        _user.save((error,data)=> {
-            if(error){
-                return res.status(400).json({
-                    message:'Somthing went wrong'
-
-                });
-            }
-            if(data)
-            {
-                return res.status(201).json({
-                    //user:data   //  to get in resp
-                    message :"user created sucessfully"
-                })
-            }
-
-        });
-
-        
-    });
+    
 
 
-});
+// });
 
 module.exports=router;
